a6d1de8cd54175540f8d7c6957bf2a0c2829d5c6
Osilir88 <osi.lih@gmail.com>
2025-01-21 12:19:45 +0100

    Actualizado

--- NaveEspacial/Nave.cs
+++ NaveEspacial/Nave.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 namespace NaveEspacial
 {
@@ -11,16 +12,20 @@ namespace NaveEspacial
     {
         public float Vida { get; set; }
         public Point Posicion { get; set; }
-        public ConsoleColor Color { get; set; } 
+        public ConsoleColor Color { get; set; }
         public Ventana VentanaC { get; set; }
+        public List<Point> PosicionesNave { get; set; }
+
 
         //Creamos el constructor
         public Nave(Point posicion, ConsoleColor color, Ventana ventana)
         {
             Posicion = posicion;           //asignamos estas variables a nuestras propiedades
-            Color = color;  
+            Color = color;
             VentanaC = ventana;
             Vida = 100;
+            PosicionesNave = new List<Point>();
+
         }
         public void Dibujar()
         {
@@ -34,6 +39,64 @@ namespace NaveEspacial
             Console.Write("<{x}>");
             Console.SetCursorPosition(x, y + 2);
             Console.Write("± W W ±");  //Los caracteres es alt + 241
+
+            PosicionesNave.Clear();
+
+            PosicionesNave.Add(new Point(x + 3, y));
+
+            PosicionesNave.Add(new Point(x + 1, y + 1));
+            PosicionesNave.Add(new Point(x + 2, y + 1));
+            PosicionesNave.Add(new Point(x + 3, y + 1));
+            PosicionesNave.Add(new Point(x + 4, y + 1));
+            PosicionesNave.Add(new Point(x + 5, y + 1));
+
+            PosicionesNave.Add(new Point(x, y + 2));
+            PosicionesNave.Add(new Point(x + 2, y + 2));
+            PosicionesNave.Add(new Point(x + 4, y + 2));
+            PosicionesNave.Add(new Point(x + 6, y + 2));
         }
+
+        public void Borrar()
+        {
+            foreach (Point item in PosicionesNave)
+            {
+                Console.SetCursorPosition(item.X, item.Y);
+                Console.Write(" ");
+
+            }
+        }
+        public void Teclado(ref Point distancia,int velocidad)
+        {
+           ConsoleKeyInfo tecla = Console.ReadKey(true);
+            if (tecla.Key == ConsoleKey.W)
+                distancia = new Point(0,-1);
+            if (tecla.Key == ConsoleKey.S)
+                distancia = new Point(0,1);
+            if (tecla.Key == ConsoleKey.D)
+                distancia = new Point(1,0);
+            if (tecla.Key == ConsoleKey.A)
+                distancia = new Point(-1,0);
+
+            distancia.X *= velocidad;
+            distancia.Y *= velocidad;
+            Posicion = new Point(Posicion.X+distancia.X,Posicion.Y+distancia.Y);
+
+        }
+        public void  Mover(int velocidad)
+        {
+            if (Console.KeyAvailable)
+            {
+                Borrar();
+                Point distancia = new Point();
+                Teclado(ref distancia,velocidad);
+
+                Dibujar();
+            }
+        }
+            
+
     }
+
 }
+
+
--- NaveEspacial/Program.cs
+++ NaveEspacial/Program.cs
@@ -3,17 +3,30 @@ using System.Drawing;
 
 Nave nave;
 Ventana ventana;
+bool jugar = true;
 
 void Iniciar()
 {
     Ventana Ventana = new Ventana(130, 50, ConsoleColor.Black, new Point(0, 0), new Point(60, 20));
-    
+
     Ventana ventana;
-    nave = new Nave(new Point(26,17), ConsoleColor.White,Ventana);
+    nave = new Nave(new Point(26, 17), ConsoleColor.White, Ventana);
     nave.Dibujar();
 }
+void Game()
+{
+    while(jugar) 
+    {
+        nave.Mover(2);
+    }
+}
+  
+        
+    
+
 
 Iniciar();
+Game();  //Llamamos al método...
 Console.ReadKey();
 
 
